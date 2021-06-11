d3988feadd521d55befab96d5bb3aa053898669a
predator-007 <srijanraj2000@gmail.com>
2021-06-11 15:17:35 +0530

    commit

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask,jsonify
 from flask import Flask,request
-from keras.models import load_model
-from keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
-from flask_cors import CORS
-
+#from flask_cors import CORS
+import tensorflow as tf
+graph=tf.compat.v1.get_default_graph()
 app = Flask(__name__)
-CORS(app)
-model=load_model("model.h5")
+#CORS(app)
 
 index = ['Fusion Beat','Normal Beat','Unknown Beat','Supraventricular ectopic Beat','Ventricular ectopic beat']
         
@@ -20,14 +20,20 @@ def working():
 
 @app.route('/predict',methods=["POST"])
 def predict_class():
-    imagef=request.files['file']
+    model=load_model("ecg_model/my_ecg_model")
+    imagef=request.files['image']
     imagen=imagef.filename
     imagef.save(os.path.join(os.getcwd(), imagen))
     img=image.load_img(imagen,target_size=(64,64))
     x=np.array(img)
     x=np.expand_dims(x,axis=0)
-    pred=model.predict_classes(x)
-    return jsonify(index[pred[0]])
+    resp=model.predict(x)
+    print(resp)    
+    resi=np.argmax(resp)
+    res=index[resi]
+    print("predictions",res)
+    return jsonify(res)
     
 if __name__=="__main__":
     app.run(debug=True,port=5000)  
+
